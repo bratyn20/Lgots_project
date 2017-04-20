dde23e7962d2e2b93b3a9941314bfee9621c43e4
bratyn20 <bratyn20@gmail.com>
2017-04-20 10:41:56 +0700

    2 комиит

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = React.createClass({
     render() {
         return
-        <h1>Льготы</h1>; 
+        <h1>Льготы </h1>; 
         <li>rfghjkk</li>; 
     }
 });
